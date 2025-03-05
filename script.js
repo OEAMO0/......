fd9ae164e9c3078278a00a46f1b7a9e8e1b645eb
OEAMO <omadkdklilipo@gmail.com>
2025-03-05 22:01:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+let div = `<article>
+        <div class="image">
+          <img src="Screenshot_-.jpg" alt="" />
+        </div>
+        <div class="title">
+          <h2>اسم الخبزة</h2>
+          <p>فوائد الخبزة</p>
+        </div>
+        <div class="button">
+          <a href="#">اضف الى السلة</a>
+          <p>9999.9$<p>
+        </div>
+      </article>`;
+let all = div.repeat(10);
+document.write(all);
